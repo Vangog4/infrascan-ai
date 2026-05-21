0eed17ab27bbdaded14be1233458836e5508ad19
InfraScan AI <vangog4@gmail.com>
2026-05-21 14:05:30 +0000

    chore: fix ruff import sort in models/__init__.py

--- addons/infrascan_ai/models/__init__.py
+++ addons/infrascan_ai/models/__init__.py
@@ -1,3 +1,3 @@
+from . import bot_report as bot_report  # noqa: F401
 from . import project_task as project_task  # noqa: F401
 from . import res_partner as res_partner  # noqa: F401
-from . import bot_report as bot_report  # noqa: F401
